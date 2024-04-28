9ca8ba8ef17a06d0a4e247ac726f1e503bfb1807
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-04-29 02:08:37 +0545

    Initial Commit

---
+++ WeatherWise Watch App/WeatherWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWiseApp.swift
+//  WeatherWise Watch App
+//
+//  Created by Paurakh Vikram Saud on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWise_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
