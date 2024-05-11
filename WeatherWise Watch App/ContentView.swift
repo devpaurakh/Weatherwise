815bb8f854f22dd6d9ed6e3a812fc9e6c4f811f8
Paurakh Saud <paurakhsaud2022@gmail.com>
2024-05-12 02:05:38 +0545

    ui changes and add scrollview

--- WeatherWise Watch App/ContentView.swift
+++ WeatherWise Watch App/ContentView.swift
@@ -1,130 +1,125 @@
-//
-//  ContentView.swift
-//  WeatherWise Watch App
-//
-//  Created by Paurakh Vikram Saud on 29/04/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @State private var showMenu = false
     var body: some View {
-        NavigationStack{
-            
-            ZStack{
+        NavigationStack {
+            ZStack {
                 Color(backgroundColor)
                     .ignoresSafeArea()
-                VStack(spacing:1,content: {
+                VStack(spacing: 1) {
                     Text("Kathmandu, Nepal")
                         .foregroundColor(.black)
                         .fontWeight(.bold)
                         .font(.system(size: 10))
                     
-                    HStack(content: {
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    HStack {
+                        Button(action: {}) {
                             Image(systemName: "chevron.left")
                                 .foregroundColor(.black)
-                        })
+                        }
                         
-                        Image("sunny")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 70,height: 70)
+                        NavigationLink(destination: WeatherDeatilPage()) {
+                            Image("sunny")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 70, height: 70)
+                        }
+                        .buttonStyle(PlainButtonStyle()) // To remove the default button style
                         
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Button(action: {}) {
                             Image(systemName: "chevron.right")
                                 .foregroundColor(.black)
-                        })
-                    })
+                        }
+                    }
                     
                     Text("42°C")
                         .foregroundColor(.black)
                         .fontWeight(.bold)
                         .font(.system(size: 28))
-                        
+                    
                     Rectangle()
-                        .frame(height:4)
+                        .frame(height: 4)
                         .background(.red)
                         .opacity(0)
                     
                     BottomItems()
-                    
-                       
-                    
-        
-                })
-               
+                }
+                
                 SideMenuView(isShowing: $showMenu)
             }
-            .toolbar{
-                ToolbarItem(placement: .topBarLeading){
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
                     Button(action: {
                         showMenu.toggle()
-                    }, label: {
+                    }) {
                         Image(systemName: "line.3.horizontal")
-                    })
+                    }
                 }
             }
         }
     }
 }
 
-#Preview {
-    ContentView()
+struct WeatherDetailPage: View {
+    var body: some View {
+        Text("Weather Detail Page")
+    }
 }
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
 
 struct BottomItems: View {
     var body: some View {
         Rectangle()
-                    .overlay {
-                        HStack(spacing:17, content: {
-                            
-                            VStack{
-                                Image(systemName: "drop")
-                                    .foregroundColor(.gray)
-                                    .font(.system(size: 20))
-                                Text("Humidity")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 10))
-                                
-                                Text("32%")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 10))
-                                    .bold()
-                            }
-                            VStack{
-                                Image(systemName: "wind")
-                                    .foregroundColor(.gray)
-                                    .font(.system(size: 25))
-                                Text("Wind")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 10))
-                                
-                                Text("23km/h")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 10))
-                                    .bold()
-                            }
-                            VStack{
-                                Image(systemName: "cloud")
-                                    .foregroundColor(.gray)
-                                    .font(.system(size: 30))
-                                Text("Precip")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 10))
-                                
-                                Text("32%")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 10))
-                                    .bold()
-                            }
-                        })
+            .overlay {
+                HStack(spacing: 17) {
+                    VStack {
+                        Image(systemName: "drop")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 20))
+                        Text("Humidity")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                        Text("32%")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                            .bold()
                     }
-                    .frame(width: 180, height:60)
-                    .background(Color.white)
-                    .cornerRadius(1)
-                    .shadow(color: Color.black.opacity(1), radius: 5, x: 0, y: 5) // Add shadow
+                    VStack {
+                        Image(systemName: "wind")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 25))
+                        Text("Wind")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                        Text("23km/h")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                            .bold()
+                    }
+                    VStack {
+                        Image(systemName: "cloud")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 30))
+                        Text("Precip")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                        Text("32%")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                            .bold()
+                    }
+                }
+            }
+            .frame(width: 180, height: 60)
+            .background(Color.white)
+            .cornerRadius(1)
+            .shadow(color: Color.black.opacity(1), radius: 5, x: 0, y: 5)
     }
 }
+
--- WeatherWise Watch App/NextdayWeatherView.swift
+++ WeatherWise Watch App/NextdayWeatherView.swift
@@ -3,9 +3,9 @@ import SwiftUI
 struct NextdayWeatherView: View {
     var body: some View {
         ZStack{
-            Color.white
+            Color(backgroundColor)
                 .ignoresSafeArea()
-           
+            
             
             ScrollView {
                 
@@ -39,7 +39,7 @@ struct NextdayWeatherView: View {
                             
                             Text("Heavy Rain")
                                 .font(.system(size: 10))
-                                
+                            
                             
                         })
                     })
@@ -48,110 +48,111 @@ struct NextdayWeatherView: View {
                     
                 }
                 
-             
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(backgroundColor)
-                        .frame(width: 160, height: 60)
-                        .offset(y: -23)
-                        .overlay {
-                            HStack(spacing:17, content: {
+                
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.white)
+                    .frame(width: 160, height: 60)
+                    .offset(y: -30)
+                    .padding()
+                    .overlay {
+                        HStack(spacing:17, content: {
+                            
+                            VStack{
+                                Image(systemName: "drop")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 20))
+                                Text("Humidity")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 8))
                                 
-                                VStack{
-                                    Image(systemName: "drop")
-                                        .foregroundColor(.gray)
-                                        .font(.system(size: 20))
-                                    Text("Humidity")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 8))
-                                    
-                                    Text("32%")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 10))
-                                        .bold()
-                                }
-                                VStack{
-                                    Image(systemName: "wind")
-                                        .foregroundColor(.gray)
-                                        .font(.system(size: 25))
-                                    Text("Wind")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 8))
-                                    
-                                    Text("23km/h")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 10))
-                                        .bold()
-                                }
-                                VStack{
-                                    Image(systemName: "cloud")
-                                        .foregroundColor(.gray)
-                                        .font(.system(size: 30))
-                                    Text("Precip")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 8))
-                                    
-                                    Text("32%")
-                                        .foregroundColor(.black)
-                                        .font(.system(size: 10))
-                                        .bold()
-                                }
-                            })
-                            .offset(y: -23)
-                        }
+                                Text("32%")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 10))
+                                    .bold()
+                            }
+                            VStack{
+                                Image(systemName: "wind")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 25))
+                                Text("Wind")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 8))
+                                
+                                Text("23km/h")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 10))
+                                    .bold()
+                            }
+                            VStack{
+                                Image(systemName: "cloud")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 30))
+                                Text("Precip")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 8))
+                                
+                                Text("32%")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 10))
+                                    .bold()
+                            }
+                        })
+                        .offset(y: -29)
+                    }
+                
+                
+                VStack(alignment: .leading, spacing:3, content: {
+                    Text("This week")
+                        .foregroundColor(.black)
+                        .font(.system(size: 10))
+                        .fontWeight(.bold)
                     
-                    
-                    VStack(alignment: .leading, spacing:3, content: {
-                        Text("This week")
+                    HStack(spacing: 20, content: {
+                        Text("Monday")
                             .foregroundColor(.black)
                             .font(.system(size: 10))
-                            .fontWeight(.bold)
                         
-                        HStack(spacing: 20, content: {
-                            Text("Monday")
-                                .foregroundColor(.black)
-                                .font(.system(size: 10))
-                            
-                            Text("28°c /18°c")
-                                .foregroundColor(.black)
-                                .font(.system(size: 10))
-                            
-                            Image(systemName: "sun.max")
-                                .foregroundColor(.black)
-                        })
+                        Text("28°c /18°c")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
                         
-                        HStack(spacing: 20, content: {
-                            Text("Monday")
-                                .foregroundColor(.black)
-                                .font(.system(size: 10))
-                            
-                            Text("28°c /18°c")
-                                .foregroundColor(.black)
-                                .font(.system(size: 10))
-                            
-                            Image(systemName: "sun.max")
-                                .foregroundColor(.black)
-                        })
-                       
-                        
-                        HStack(spacing: 20, content: {
-                            Text("Monday")
-                                .foregroundColor(.black)
-                                .font(.system(size: 10))
-                            
-                            Text("28°c /18°c")
-                                .foregroundColor(.black)
-                                .font(.system(size: 10))
-                            
-                            Image(systemName: "sun.max")
-                                .foregroundColor(.black)
-                            
-                        })
+                        Image(systemName: "sun.max")
+                            .foregroundColor(.black)
                     })
-                    .offset(y: -20)
-                }
+                    
+                    HStack(spacing: 20, content: {
+                        Text("Monday")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                        
+                        Text("28°c /18°c")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                        
+                        Image(systemName: "sun.max")
+                            .foregroundColor(.black)
+                    })
+                    
+                    
+                    HStack(spacing: 20, content: {
+                        Text("Monday")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                        
+                        Text("28°c /18°c")
+                            .foregroundColor(.black)
+                            .font(.system(size: 10))
+                        
+                        Image(systemName: "sun.max")
+                            .foregroundColor(.black)
+                        
+                    })
+                })
+                .offset(y: -30)
             }
         }
     }
+}
 
 
 #Preview {
